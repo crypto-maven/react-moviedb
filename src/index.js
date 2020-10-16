bb50ef7bd6cea133c80dbf59da984f4ba8bebe58
Nicole Villa <nicolevilla.109@gmx.com>
2020-10-16 01:22:27 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
       <h1 className="title">React Movie Search</h1>
+    
       </div>
     );
   }
--- src/test.js
+++ src/test.js
@@ -4,3 +4,9 @@ export default function Test(){
         <h1>Hello World</h1>
     )
 }
+
+// to create the SearchMovies component
+// form with a class of form
+// label with htmlFor="query" and a class of label
+// input of type text with a name of "query" and a placeholder
+// button class of button and a type of submit
